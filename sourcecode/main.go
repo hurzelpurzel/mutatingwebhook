d3183a9bce10496063fc3dc85a47ea9537504cc6
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2022-05-21 08:29:05 +0200

    change to ubi

--- sourcecode/main.go
+++ sourcecode/main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"log"
 	"net/http"
 
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
 	"fmt"
-	"k8s.io/client-go/kubernetes"
-	rest "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"os"
-	"path/filepath"
 
 	"flag"
 	"io/ioutil"
 	"strconv"
 
 	"errors"
+
 	"k8s.io/api/admission/v1beta1"
 
 	"encoding/json"
+
 	apiv1 "k8s.io/api/core/v1"
 )
 
@@ -43,54 +40,13 @@ var (
 	universalDeserializer = serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer()
 )
 
-var config *rest.Config
-var clientSet *kubernetes.Clientset
-
 func main() {
-
-	useKubeConfig := os.Getenv("USE_KUBECONFIG")
-	kubeConfigFilePath := os.Getenv("KUBECONFIG")
 
 	flag.IntVar(&parameters.port, "port", 8443, "Webhook server port.")
 	flag.StringVar(&parameters.certFile, "tlsCertFile", "/etc/webhook/certs/tls.crt", "File containing the x509 Certificate for HTTPS.")
 	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/webhook/certs/tls.key", "File containing the x509 private key to --tlsCertFile.")
 	flag.Parse()
 
-	if len(useKubeConfig) == 0 {
-		// default to service account in cluster token
-		c, err := rest.InClusterConfig()
-		if err != nil {
-			panic(err.Error())
-		}
-		config = c
-	} else {
-		//load from a kube config
-		var kubeconfig string
-
-		if kubeConfigFilePath == "" {
-			if home := homedir.HomeDir(); home != "" {
-				kubeconfig = filepath.Join(home, ".kube", "config")
-			}
-		} else {
-			kubeconfig = kubeConfigFilePath
-		}
-
-		fmt.Println("kubeconfig: " + kubeconfig)
-
-		c, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			panic(err.Error())
-		}
-		config = c
-	}
-
-	cs, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	clientSet = cs
-
-	
 	http.HandleFunc("/", HandleRoot)
 	http.HandleFunc("/mutate", HandleMutate)
 	log.Fatal(http.ListenAndServeTLS(":"+strconv.Itoa(parameters.port), parameters.certFile, parameters.keyFile, nil))
